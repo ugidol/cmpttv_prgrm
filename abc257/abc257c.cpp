3fa856dc86d459584f861010f3cea3e62d7eb202
ugidol <hirata20070626@gmail.com>
2022-07-10 17:14:36 +0900

    ac

--- abc257/abc257c.cpp
+++ abc257/abc257c.cpp
@@ -1,50 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N;
-//set<int> total;
-vector<int> total;
-multiset<int> adult;
-multiset<int> child;
-int func(int w){
-    auto childp = lower_bound(child.begin(), child.end(), w);
-    // 実際の子供の人数
-    int num_of_child = distance(child.begin(), childp);
-
-    auto adultp = lower_bound(adult.begin(), adult.end(), w);
-    // 実際の大人の人数
-    int num_of_adult = adult.size() - distance(adult.begin(),adultp);
-
-    return num_of_child + num_of_adult ;
-}
 int main(void){
+    int N;
     cin >> N;
     string S;
     cin >> S;
     vector<int> W(N);
-    int mx = 0;
+    map<int,vector<int>> mp;
     for(int i=0;i<N;i++){
-        cin >> W[i];
-        total.push_back(W[i]);
-        // '0' は子供
-        if ( S[i] == '0' ) {
-            child.insert(W[i]);
-        }
-        else {
-            adult.insert(W[i]);
-        }
-        //
-        mx = max(mx,W[i]);
-    }
-    total.push_back(mx+1);
-    sort(total.begin(), total.end());
-    //
-    int ans = 0;
-    for(auto w : total ) {
-        int tmpans = func(w);
-        ans = max({ans, tmpans });
+        int w;
+        cin >> w;
+        W[i] = w;
+        mp[w].push_back(i);
     }
     //
-
+    int tmp = 0;
+    for(int i=0;i<N;i++){
+        if ( S[i] == '1' ) {
+            tmp += 1;
+        }
+    }
+    int ans = tmp;
+    for(auto p : mp ) {
+        for(auto it : p.second) {
+            if ( S[it] == '1' ) {
+                tmp -= 1;
+            }
+            else {
+                tmp += 1;
+            }
+        }
+        ans = max(ans, tmp);
+    }
     cout << ans << endl;
     return 0;
 }
